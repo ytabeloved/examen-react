2bcbbed665198f3db256c2bc56673ea19b502fb2
Karla Vergara <karlisvaldi@gmail.com>
2026-05-18 20:54:49 -0400

    Fix NavBar final

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,19 +1,13 @@
 function Navbar() {
-
   return (
-
     <nav className="navbar navbar-expand-lg navbar-dark navbar-custom shadow">
-
       <div className="container">
-
         <a className="navbar-brand fw-bold" href="#">
           TechStore
         </a>
 
         <div>
-
           <ul className="navbar-nav ms-auto flex-row gap-3">
-
             <li className="nav-item">
               <a className="nav-link active" href="#">
                 Inicio
@@ -31,17 +25,11 @@ function Navbar() {
                 Contacto
               </a>
             </li>
-
           </ul>
-
         </div>
-
       </div>
-
     </nav>
-
   );
-
 }
 
 export default Navbar;
